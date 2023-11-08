6979dae19f794bf96fe6392d0a75d216b9bf3550
Aarnav Tale <aarnavtale@icloud.com>
2023-11-07 22:40:07 -0500

    fix: do glDisable cleanups

--- src/term/destroy.c
+++ src/term/destroy.c
@@ -7,6 +7,9 @@ void destroy_term(term_t *state) {
 	FT_Done_Face(state->font.face);
 	FT_Done_FreeType(state->font.library);
 
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_BLEND);
+
 	SDL_DestroyWindow(state->window);
 	SDL_Quit();
 }
