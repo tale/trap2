342b3b4fd8f69e938a0074f5c8e79b5483220fdc
Aarnav Tale <aarnavtale@icloud.com>
2023-12-17 02:04:49 -0500

    fix: mac.m is ignored on non macos

--- src/platform/mac.m
+++ src/platform/mac.m
@@ -1,11 +1,16 @@
 #include "mac.h"
+
+#ifdef __APPLE__
 #import <AppKit/AppKit.h>
 #import <Foundation/Foundation.h>
+#endif
 
 void darkenTitlebar(void) {
+#ifdef __APPLE__
 	if (@available(macOS 10.14, *)) {
 		NSAppearance *appearance = [NSAppearance appearanceNamed:NSAppearanceNameDarkAqua];
 		NSApplication *app = [NSApplication sharedApplication];
 		[app setAppearance:appearance];
 	}
+#endif
 }
